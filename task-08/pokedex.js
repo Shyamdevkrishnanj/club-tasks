62b9a426e24291088b1fb07ede03a0f26cb309a7
Shyamdevkrishnanj <sdkrishnan16@gmail.com>
2023-03-28 09:25:07 +0530

    Task-06 Updated

---
+++ task-08/pokedex.js
@@ -0,0 +1,64 @@
+const pokedex = document.getElementById('pokedex');
+
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 20; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            id: result.id
+        }));
+        displayPokemon(pokemon);
+    });
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon
+        .map(
+            (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
+
+var playBtn = document.getElementById('play'),
+  resetBtn = document.getElementById('reset'),
+  hearbeat = document.getElementById('heartbeat')
+	audios = document.querySelectorAll('audio');
+console.log(audios);
+
+
+playBtn.addEventListener('mouseover', function() {
+[].forEach.call(audios, function(audio) {
+  // do whatever
+  audio.play();
+});
+}, false);
+
+playBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
+
+resetBtn.addEventListener('mouseover', function() {
+    heartbeat.play();
+}, false);
+
+resetBtn.addEventListener('mouseleave', function() {
+  heartbeat.pause();
+  heartbeat.currentTime = 0;
+}, false);
